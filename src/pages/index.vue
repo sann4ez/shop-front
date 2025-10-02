7b05e182fff58e1f84ea28e7e647a57dcc23d89f
Olexandr Kyrychuk <kyrychuk.oleksandr@itspace.company>
2025-10-02 21:50:17 +0300

    add card & upd swiper

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,14 +9,7 @@ import "swiper/css";
 import "swiper/css/effect-fade";
 import "swiper/css/pagination";
 
-const onSwiper = (swiper: any) => {
-  console.log(swiper);
-};
-
-const onSlideChange = () => {
-  console.log("slide change");
-};
-
+// Дані з API
 const slides = [
   "https://picsum.photos/id/1015/1920/800",
   "https://picsum.photos/id/1016/1920/800",
@@ -26,35 +19,35 @@ const slides = [
 
 const products = [
   {
-    name: "Товар 1",
+    name: "Сучасний металевий стілець",
     price: 1200,
     priceOld: 1400,
     article: "ART-0001",
     image: "https://picsum.photos/300/300?1",
   },
   {
-    name: "Товар 2",
+    name: "Великий обідній стіл з натурального дуба на 6 персон із захисним лакованим покриттям та масивними ніжками",
     price: 1500,
     priceOld: 1600,
     article: "ART-0002",
     image: "https://picsum.photos/300/300?2",
   },
   {
-    name: "Товар 3",
+    name: "Настільна керамічна лампа з декоративною основою та текстильним абажуром кольору молочного льону для спальні та вітальні",
     price: 1000,
     priceOld: 1300,
     article: "ART-0003",
     image: "https://picsum.photos/300/300?3",
   },
   {
-    name: "Товар 4",
+    name: "Комплект кухонних ножів",
     price: 1800,
     priceOld: 1950,
     article: "ART-0004",
     image: "https://picsum.photos/300/300?4",
   },
   {
-    name: "Товар 5",
+    name: "Великий двосторонній плед із мікрофібри з утепленим шаром для спальні та вітальні, розмір 220x240 см, колір світло-бежевий",
     price: 2000,
     priceOld: 0,
     article: "ART-0005",
@@ -88,72 +81,23 @@ const products = [
       </div>
     </section>
 
-    <section class="new-arrivals">
-      <div class="container">
-        <div class="new-arrivals__header">
-          <span class="card__title new-arrivals__title">Новинки</span>
+    <ProductSection
+      title="Новинки"
+      :products="products"
+      @add-to-cart="handleAddToCart"
+    />
 
-          <!-- Кнопки навігації -->
-          <div class="new-arrivals__nav">
-            <button class="swiper-button-prev custom-prev"></button>
-            <button class="swiper-button-next custom-next"></button>
-          </div>
-        </div>
+    <ProductSection
+      title="Розпродаж"
+      :products="products"
+      @add-to-cart="handleAddToCart"
+    />
 
-        <div class="new-arrivals__slider">
-          <Swiper
-            :modules="[Navigation, Autoplay]"
-            :slides-per-view="4"
-            :space-between="20"
-            :loop="true"
-            :navigation="{ nextEl: '.custom-next', prevEl: '.custom-prev' }"
-            :autoplay="{ delay: 300000 }"
-            class="new-arrivals__swiper"
-          >
-            <SwiperSlide
-              v-for="(product, index) in products"
-              :key="index"
-              class="new-arrivals__slide"
-            >
-              <a :href="'/product/' + product.article" class="product-card">
-                <img
-                  :src="product.image"
-                  :alt="product.name"
-                  class="product-card__img"
-                />
-                <div class="product-content">
-                  <p class="product-card__article">{{ product.article }}</p>
-                  <h3 class="product-card__name">{{ product.name }}</h3>
-
-                  <div class="product-card__bottom">
-                    <div class="product-card__prices">
-                      <span
-                        v-if="product.priceOld"
-                        class="product-card__price-old"
-                      >
-                        {{ product.priceOld }} ₴
-                      </span>
-                      <span class="product-card__price"
-                        >{{ product.price }} ₴</span
-                      >
-                    </div>
-
-                    <button
-                      type="button"
-                      class="product-card__add-to-cart"
-                      @click.prevent="$emit('add-to-cart', product)"
-                    >
-                      Додати в кошик
-                    </button>
-                  </div>
-                </div>
-              </a>
-            </SwiperSlide>
-          </Swiper>
-        </div>
-      </div>
-    </section>
-
+    <ProductSection
+      title="Популярні"
+      :products="products"
+      @add-to-cart="handleAddToCart"
+    />
     <slot />
   </main>
 
@@ -195,86 +139,5 @@ const products = [
 
 .swiper-button-prev {
   rotate: 180deg;
-}
-
-// Новинки
-.new-arrivals {
-  padding: 70px 0 70px 0;
-
-  &__header {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-bottom: 10px;
-    padding: 0 20px;
-  }
-
-  &__nav {
-    display: flex;
-    gap: 10px;
-
-    .swiper-button-prev,
-    .swiper-button-next {
-      position: static;
-      width: 50px;
-      height: 50px;
-      border: 1px solid $bg-color-brown;
-      border-radius: 5px;
-      cursor: pointer;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      font-size: 18px;
-      transition: all 0.3s ease;
-
-      &:hover {
-        background: $bg-color-brown;
-
-        :deep(.swiper-navigation-icon),
-        :deep(.swiper-navigation-icon) {
-          color: #fff;
-        }
-      }
-    }
-  }
-}
-
-.product-card__img {
-  width: 100%;
-}
-
-.product-content {
-  margin-top: 5px;
-}
-
-.product-card__article,
-.product-card__name {
-  margin-bottom: 10px;
-}
-
-.product-card__article {
-  font-weight: 500;
-  color: $text-color-grey;
-}
-
-.product-card__name {
-  font-weight: 700;
-}
-
-.new-arrivals__slider {
-  padding-top: 20px;
-}
-
-.new-arrivals__slide {
-  padding: 0 20px;
-  border: 1px solid $bg-color-brown;
-  -webkit-box-shadow: 0px 0px 10px 1px #000000;
-  box-shadow: 0 0 10px 1px #000000;
-}
-
-.product-card__bottom {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
 }
 </style>
